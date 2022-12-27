a3c8aab83e82c3f108522e3fd35c3f3d6368f7de
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2022-12-27 16:42:00 +0300

    init project

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+{
+  "extends": ["@hipo/stylelint-config-base"],
+  "plugins": [
+    "stylelint-order",
+    "stylelint-scss",
+    "stylelint-no-unsupported-browser-features"
+  ],
+  "rules": {
+    "value-list-comma-newline-after": null,
+    "font-family-name-quotes": null,
+    "function-parentheses-space-inside": "never-single-line",
+    "function-comma-space-after": "always-single-line"
+  }
+}
